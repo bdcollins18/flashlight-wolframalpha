039f99bee6dc084456cb19ae923daac0af05f760
Brian Collins <bdcollins18@gmail.com>
2020-06-04 02:09:48 -0400

    Working plugin.py
    reads appid from file, requests info and shows it in unstyled html

--- wolframalpha.bundle/plugin.py
+++ wolframalpha.bundle/plugin.py
@@ -1,6 +1,27 @@
+import requests
+from urllib import quote
+import webbrowser
+
+api_url = "http://api.wolframalpha.com/v2/result"
+appid_file = open("appid.txt", "r")
+appid = appid_file.read()
+appid_file.close()
+params = {"appid": appid}
+
+
 def results(fields, original_query):
-	input = fields['~input']
-	return {
-		"title": "TEST input='{0}'".format(input),
-		"run_args": [input]
-	}
+    input_string = fields["~input"]
+    params["i"] = input_string
+    r = requests.get(url=api_url, params=params)
+
+    return {
+        "title": "Wolfram Alpha",
+        "html": r.text,
+        "webview_transparent_background": True,
+        "run_args": [input_string]
+    }
+
+site_url = "https://www.wolframalpha.com/input/?i="
+
+def run(input_string) :
+    webbrowser.open(site_url + quote(input_string))
