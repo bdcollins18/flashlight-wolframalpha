b5b55d9335788ec90cbfeeb13222c6774831463b
Brian Collins <bdcollins18@gmail.com>
2020-06-04 03:38:34 -0400

    Pretty output!

--- wolframalpha.bundle/plugin.py
+++ wolframalpha.bundle/plugin.py
@@ -8,15 +8,18 @@ appid = appid_file.read()
 appid_file.close()
 params = {"appid": appid}
 
+html_file = open("html.html", "r")
+html_text = html_file.read()
+html_file.close()
+
 
 def results(fields, original_query):
     input_string = fields["~input"]
     params["i"] = input_string
     r = requests.get(url=api_url, params=params)
-
     return {
         "title": "Wolfram Alpha",
-        "html": r.text,
+        "html": html_text.replace("<!--INPUT-->", input_string).replace("<!--ANSWER-->", r.text),
         "webview_transparent_background": True,
         "run_args": [input_string]
     }
